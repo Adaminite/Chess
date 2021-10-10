9ec8a1a4a7b33caa66395d887bc8d8599b510946
Adaminite <73565393+Adaminite@users.noreply.github.com>
2021-10-10 13:20:15 -0400

    Add files via upload

--- src/niazi/INTERFACES/Movable.java
+++ src/niazi/INTERFACES/Movable.java
@@ -7,5 +7,4 @@ import niazi.PIECES.*;
 public interface Movable {
 	public abstract ChessPiece[][] move(int[] location, ChessPiece[][] board);
 	public abstract ArrayList <int[]> getPossibleMoves(ChessPiece[][] board, boolean makingAMove);
-	//public abstract ChessPiece[][] capture();
 }
